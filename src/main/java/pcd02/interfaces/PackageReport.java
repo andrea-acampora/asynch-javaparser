ff8efbf301c1391ecd660ef65352b40f11ed8023
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-04-26 14:09:25 +0200

    feat: create interface for package reports

--- src/main/java/pcd02/interfaces/PackageReport.java
+++ src/main/java/pcd02/interfaces/PackageReport.java
@@ -1,15 +1,22 @@
 package pcd02.interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PackageReport {
 
 	String getFullPackageName();
-	
-	String getSrcFullFileName();
 
 	List<InterfaceReport> getInterfaceInfo();
 
 	List<ClassReport> getClassInfo();
+
+	Optional<ClassReport> getMainClass();
+
+	void setFullPackageName(String fullPackageName);
+
+	void addInterfaceInfo(InterfaceReport interfaceReport);
+
+	void addClassInfo(ClassReport classReport);
 	
 }
